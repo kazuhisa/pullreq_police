0965c857b0eb6f5fcbd86f10e55de3990b4fc1ff
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2017-12-15 10:41:02 +0900

    プルリク作成時にOctokit::BadGatewayが発生したときの処理を追加

--- pullreq.rb
+++ pullreq.rb
@@ -5,10 +5,14 @@ setting = YAML.load_file('setting.yml')
 client = Octokit::Client.new(access_token: setting['github_token'])
 list = setting['branches']
 (list.size - 1).times.each do |idx|
+  retry_counter = 0
   begin
     from_branch = list[idx]
     to_branch = list[idx + 1]
     client.create_pull_request(setting['repos'], to_branch, from_branch, setting['title'], setting['body'])
+  rescue Octokit::BadGateway
+    retry_counter += 1
+    retry if retry_counter < 3
   rescue Octokit::UnprocessableEntity => e
     puts e.message
   end
